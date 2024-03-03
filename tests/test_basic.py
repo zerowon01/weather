88abc524ab3c55eb6127cc35482e55d5a4087e20
Greig Bloom <greig.bloom@amelia.com>
2024-03-04 08:24:03 +0900

    Break test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,5 +4,5 @@ from weather.cli import cli
 
 def test_cli():
     runner = CliRunner()
-    result = runner.invoke(cli, auto_envvar_prefix="WEATHER")
+    result = runner.invoke(cli, auto_envvar_prefix="TWEATHER")
     assert result.exit_code == 0
