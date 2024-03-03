ea5eecae3ae352d5ce1419eb143565341faac7b7
Greig Bloom <greig.bloom@amelia.com>
2024-03-04 08:25:06 +0900

    Fix failing test.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,5 +4,5 @@ from weather.cli import cli
 
 def test_cli():
     runner = CliRunner()
-    result = runner.invoke(cli, auto_envvar_prefix="TWEATHER")
+    result = runner.invoke(cli, auto_envvar_prefix="WEATHER")
     assert result.exit_code == 0
